39be481495e32ab276720a5ec1746165365f2d94
IgorBoyarincev <boyarincev.igor@list.ru>
2022-10-04 13:31:33 +0300

    new version

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -3,8 +3,8 @@ import java.util.Comparator;
 public class PersonComparator implements Comparator<Person> {
     @Override
     public int compare(Person p1, Person p2) {
-        int len1 = p1.getSurname().split(" ").length;
-        int len2 = p2.getSurname().split(" ").length;
+        int len1 = p1.getSurname().split("\\P{IsAlphabetic}+").length;
+        int len2 = p2.getSurname().split("\\P{IsAlphabetic}+").length;
         if (len1 != len2) {
             return Integer.compare(len1, len2);
         }
